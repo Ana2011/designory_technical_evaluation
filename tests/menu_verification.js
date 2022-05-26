307500c855bfbed18f2552409652397a9f941d5b
Anastasiia <nastencheg@gmail.com>
2022-05-26 13:28:34 -0500

    added tests for rest of the pages

--- tests/menu_verification.js
+++ tests/menu_verification.js
@@ -18,6 +18,38 @@ module.exports = {
             .checkPage("work")
             .assert.urlContains("work")
             .assert.titleEquals("Designory Work & Case Studies");
+    },
+
+    "Verifying About": function(browser){
+        browser.page.degignoryPage()
+            .checkPage("about")
+            .assert.urlContains("about")
+            .assert.titleEquals("Print & Digital Agency | Designory");
+    },
+
+    "Verifying Careers": function(browser){
+        browser.page.degignoryPage()
+            .checkPage("careers")
+            .assert.urlContains("careers")
+            .assert.titleEquals("Careers | Designory");
+    },
+
+    // "Verifying Locations": function(browser){
+    //     // TO CHECK WHAT OT DO
+    // },
+
+    "Verifying Contact": function(browser){
+        browser.page.degignoryPage()
+            .checkPage("contact")
+            .assert.urlContains("contact")
+            .assert.titleEquals("Contact | Designory");
+    },
+
+    "Verifying News": function(browser){
+        browser.page.degignoryPage()
+            .checkPage("news")
+            .assert.urlContains("news")
+            .assert.titleEquals("Designory News & Awards | Designory");
     }
 
 }
