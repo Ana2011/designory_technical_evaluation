40a143288c81dc1cb11ee1e3fea87629425b19c7
Anastasiia <nastencheg@gmail.com>
2022-05-26 22:56:30 -0500

    renamed designoryPage

--- tests/cookie_verification.js
+++ tests/cookie_verification.js
@@ -1,21 +1,21 @@
 module.exports = {
 
     "Verify Accept": function(browser){
-        browser.page.degignoryPage()
+        browser.page.designoryPage()
             .checkCookieGot()
             .assert.not.visible('@cookieContainer')
             .end();
     },
 
     "Verify X": function(browser){
-        browser.page.degignoryPage()
+        browser.page.designoryPage()
             .checkCookieX()
             .assert.not.visible('@cookieContainer')
             .end();
     },
 
     "Verify Clean": function(browser){
-        browser.page.degignoryPage()
+        browser.page.designoryPage()
             .checkCookieClean()
             .assert.visible('@cookieContainer')
             .end();
--- tests/menu_verification.js
+++ tests/menu_verification.js
@@ -1,12 +1,12 @@
 module.exports = {
 
     beforeEach: function(browser){
-        browser.page.degignoryPage()
+        browser.page.designoryPage()
             .navigate()
     },
 
     afterEach : function(browser) {
-        browser.page.degignoryPage()
+        browser.page.designoryPage()
             .checkMenu()
     },
 
@@ -19,42 +19,42 @@ module.exports = {
     },
 
     "Verifying Work": function(browser){
-        browser.page.degignoryPage()
+        browser.page.designoryPage()
             .checkPage("work")
             .assert.urlContains("work")
             .assert.titleEquals("Designory Work & Case Studies");
     },
 
     "Verifying About": function(browser){
-        browser.page.degignoryPage()
+        browser.page.designoryPage()
             .checkPage("about")
             .assert.urlContains("about")
             .assert.titleEquals("Print & Digital Agency | Designory");
     },
 
     "Verifying Careers": function(browser){
-        browser.page.degignoryPage()
+        browser.page.designoryPage()
             .checkPage("careers")
             .assert.urlContains("careers")
             .assert.titleEquals("Careers | Designory");
     },
 
     "Verifying Locations": function(browser){
-        browser.page.degignoryPage()
+        browser.page.designoryPage()
             .checkPageLocation()
             .assert.urlContains("location")
             .assert.titleEquals("Long Beach Advertising Agency | Designory");
     },
 
     "Verifying Contact": function(browser){
-        browser.page.degignoryPage()
+        browser.page.designoryPage()
             .checkPage("contact")
             .assert.urlContains("contact")
             .assert.titleEquals("Contact | Designory");
     },
 
     "Verifying News": function(browser){
-        browser.page.degignoryPage()
+        browser.page.designoryPage()
             .checkPage("news")
             .assert.urlContains("news")
             .assert.titleEquals("Designory News & Awards | Designory");
